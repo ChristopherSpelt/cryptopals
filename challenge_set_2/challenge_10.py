14653cde8fd048190dd015c41636fb8d5feceae6
Christopher Spelt <christopherspelt@icloud.com>
2022-06-13 11:22:02 +0200

    finished challenge 10

--- challenge_set_2/challenge_10.py
+++ challenge_set_2/challenge_10.py
@@ -1,57 +1,71 @@
 import base64
+import sys
 from posixpath import split
 from pydoc import plain
 from typing import List
-import sys
 
 sys.path.append("../challenge_set_1/")
 from challenge_7 import decrypt_AES_ECB, encrypt_AES_ECB
+
 from challenge_9 import pad_block
+
 
 def xor(x: bytes, y: bytes) -> bytes:
     if len(x) != len(y):
-        print(f"Error: lengths of input must be equal, but got lengths {len(x)} and {len(y)} ")
-    return bytes([a^b for (a,b) in zip(x,y)])
+        print(
+            f"Error: lengths of input must be equal, but got lengths {len(x)} and {len(y)} "
+        )
+    return bytes([a ^ b for (a, b) in zip(x, y)])
+
 
 def split_into_blocks(input: bytes, blocksize: int) -> List[bytes]:
     if blocksize > 256:
         print("Error: cannot exceed maximum blocksize of 256")
         return [""]
 
-    blocks = [input[i: i+blocksize] for i in range(0, len(input), blocksize)]
-    
+    blocks = [input[i : i + blocksize] for i in range(0, len(input), blocksize)]
+
     if blocks[-1] == blocksize:
-        blocks.append(blocksize.to_bytes(1, byteorder='big')*blocksize)
+        blocks.append(blocksize.to_bytes(1, byteorder="big") * blocksize)
         return blocks
-    
+
     blocks[-1] = pad_block(blocks[-1], blocksize)
     return blocks
-    
-  
-def encrypt_in_CBC_mode(plaintext: bytes, key: bytes, initialization_vector: bytes) -> bytes:
+
+
+def encrypt_in_CBC_mode(
+    plaintext: bytes, key: bytes, initialization_vector: bytes
+) -> bytes:
     block_length = len(key)
     blocks = split_into_blocks(plaintext, block_length)
 
-    print(xor(blocks[0],initialization_vector))
-    cypher_text = []
-    blocks[0] = xor(blocks[0], initialization_vector)
-    for i in range(1,len(blocks)):
-        cypher_text.append(blocks[i])
-    return blocks
+    blocks[0] = encrypt_AES_ECB(xor(blocks[0], initialization_vector), key)
+    for i in range(1, len(blocks)):
+        blocks[i] = encrypt_AES_ECB(xor(blocks[i - 1], blocks[i]), key)
+    return b"".join(blocks)
 
 
-def decrypt_in_CBC_mode(cypthertext: bytes, key, initialization_vector: bytes) -> bytes:
-    pass
+def decrypt_in_CBC_mode(
+    cyphertext: bytes, key: bytes, initialization_vector: bytes
+) -> bytes:
+    block_length = len(key)
+    blocks = split_into_blocks(cyphertext, block_length)
+
+    plaintext = [b""] * len(blocks)
+    plaintext[0] = xor(decrypt_AES_ECB(blocks[0], key), initialization_vector)
+    for i in range(1, len(blocks)):
+        plaintext[i] = xor(blocks[i - 1], decrypt_AES_ECB(blocks[i], key))
+
+    return b"".join(plaintext)
+
 
 if __name__ == "__main__":
-    with open('data/challenge_10.txt') as file:
+    with open("data/challenge_10.txt") as file:
         cyphertext = base64.b64decode(file.read())
-    
-    with open('data/challenge_10_1.txt') as file:
-        plaintext = bytes(file.read(), encoding='ascii')
-    
 
-    key = b"YELLOW SUBMARINE" 
-    iv = b"\x00" * len(key) 
-    cyphertext = encrypt_in_CBC_mode(plaintext=plaintext, key=key, initialization_vector=iv)
-    print(cyphertext)
+    key = b"YELLOW SUBMARINE"
+    iv = b"\x00" * len(key)
+    plaintext = decrypt_in_CBC_mode(
+        cyphertext=cyphertext, key=key, initialization_vector=iv
+    )
+    print(plaintext)
