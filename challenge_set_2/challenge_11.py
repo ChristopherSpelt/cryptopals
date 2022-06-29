13c47d875a742e8f230345efee749e2b9ecd5687
Christopher Spelt <christopherspelt@icloud.com>
2022-06-29 13:30:56 +0200

    small refactoring

--- challenge_set_2/challenge_11.py
+++ challenge_set_2/challenge_11.py
@@ -1,7 +1,6 @@
 import random
 import secrets
 import sys
-from typing import Optional, Tuple
 
 sys.path.append("../challenge_set_1/")
 from challenge_7 import encrypt_AES_ECB
@@ -9,23 +8,16 @@ from challenge_7 import encrypt_AES_ECB
 from challenge_10 import encrypt_AES_CBC
 
 
-def encryption_oracle(
-    input: bytes,
-    key: Optional[bytes] = None,
-    only_ECB: bool = False,
-    append: bool = False,
-) -> bytes:
-    if not key:
-        key = secrets.token_bytes(16)
-    if append:
-        append_len_1 = random.randint(5, 10)
-        append_len_2 = random.randint(5, 10)
-        append_1 = secrets.token_bytes(append_len_1)
-        append_2 = secrets.token_bytes(append_len_2)
-        input = append_1 + input + append_2
+def encryption_oracle(input: bytes) -> bytes:
+    key = secrets.token_bytes(16)
+    append_len_1 = random.randint(5, 10)
+    append_len_2 = random.randint(5, 10)
+    append_1 = secrets.token_bytes(append_len_1)
+    append_2 = secrets.token_bytes(append_len_2)
+    input = append_1 + input + append_2
 
     encryption_method = random.randint(0, 1)
-    if not only_ECB and encryption_method == 0:
+    if encryption_method == 0:
         iv = secrets.token_bytes(len(key))
         return encrypt_AES_CBC(plaintext=input, key=key, initialization_vector=iv)
     else:
@@ -33,7 +25,6 @@ def encryption_oracle(
 
 
 def detect_ECB_encryption_mode(input: bytes, keysize: int) -> bool:
-
     count = 0
     for i in range(0, len(input), keysize):
         substring = input[i : i + 16]
@@ -50,7 +41,7 @@ if __name__ == "__main__":
     with open("data/challenge_10_1.txt") as file:
         plaintext = bytes(file.read(), encoding="ascii")
 
-    cyphertext = encryption_oracle(plaintext, append=True)
+    cyphertext = encryption_oracle(plaintext)
     print(
         "ECB detected"
         if detect_ECB_encryption_mode(cyphertext, 16)
--- challenge_set_2/challenge_12.py
+++ challenge_set_2/challenge_12.py
@@ -1,18 +1,30 @@
 import base64
 import string
 import secrets
+import sys
+from typing import Callable
 
-from challenge_11 import encryption_oracle, detect_ECB_encryption_mode
+sys.path.append("../challenge_set_1/")
+from challenge_7 import encrypt_AES_ECB
+
+from challenge_11 import detect_ECB_encryption_mode
+
 
 RANDOM_KEY = secrets.token_bytes(16)
+with open("data/challenge_12.txt") as file:
+    UNKOWN_STRING = base64.b64decode(file.read())
 
 
-def detect_block_size(max: int = 128) -> int:
+def ECB_encryption_oracle(mystring: bytes) -> bytes:
+    plaintext = mystring + UNKOWN_STRING
+    return encrypt_AES_ECB(plaintext=plaintext, key=RANDOM_KEY)
+
+
+def detect_block_size(oracle: Callable[[bytes], bytes], max: int = 128) -> int:
     mystring = b"A"
-
-    previous_len = len(ECB_encryption_oracle(mystring))
+    previous_len = len(oracle(mystring))
     for _ in range(1, max):
-        cyphertext = ECB_encryption_oracle(mystring)
+        cyphertext = oracle(mystring)
         current_len = len(cyphertext)
         if current_len > previous_len:
             return current_len - previous_len
@@ -20,34 +32,30 @@ def detect_block_size(max: int = 128) -> int:
         mystring += b"A"
 
 
-def detect_ECB(blocksize: int) -> bool:
+def detect_ECB(oracle: Callable[[bytes], bytes], blocksize: int) -> bool:
     mystring = b"A" * 1024
-    cyphertext = ECB_encryption_oracle(mystring)
+    cyphertext = oracle(mystring)
     return detect_ECB_encryption_mode(cyphertext, blocksize)
 
 
-def ECB_encryption_oracle(mystring: bytes) -> bytes:
-    with open("data/challenge_12.txt") as file:
-        unknown_string = base64.b64decode(file.read())
-    plaintext = mystring + unknown_string
-    return encryption_oracle(input=plaintext, key=RANDOM_KEY, only_ECB=True)
-
-
-if __name__ == "__main__":
-    blocksize = detect_block_size()
-    print(f"blocksize is {blocksize}")
-    print(f"oracle using ECB: {detect_ECB(blocksize)}\n")
-
-    cyphertext = ECB_encryption_oracle(b"")
+def decrypt_ECB_oracle(oracle: Callable[[bytes], bytes], blocksize: int) -> bytes:
+    cyphertext = oracle(b"")
     decypted_message = b""
     text_size = len(cyphertext)
     for i in range(text_size, 1, -1):
         mystring = b"A" * (i - 1)
-        cyphertext = ECB_encryption_oracle(mystring)[text_size - blocksize : text_size]
+        cyphertext = oracle(mystring)[text_size - blocksize : text_size]
         for char in string.printable:
             teststring = mystring + decypted_message + bytes(char, "ascii")
-            out = ECB_encryption_oracle(teststring)[text_size - blocksize : text_size]
+            out = oracle(teststring)[text_size - blocksize : text_size]
             if out == cyphertext:
                 decypted_message += bytes(char, "ascii")
                 break
-    print(decypted_message)
+    return decypted_message
+
+
+if __name__ == "__main__":
+    blocksize = detect_block_size(ECB_encryption_oracle)
+    print(f"blocksize is {blocksize}")
+    print(f"oracle using ECB: {detect_ECB(ECB_encryption_oracle, blocksize)}")
+    print(f"decrypted text is: {decrypt_ECB_oracle(ECB_encryption_oracle, blocksize)}")
